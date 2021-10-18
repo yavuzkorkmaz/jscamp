0acd400aea6135ce986ab04a0130ac5e48db6399
yavuzkorkmaz <77725481+yavuzkorkmaz@users.noreply.github.com>
2021-10-19 00:11:12 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("Merhaba Kodlama.io")
+
+//undefined
+//not type safe
+//bu genelde backend sunucusundan istek alır ordan gelir
+//const da bir şey atayınca değiştiremezsin
+
+let dolarBugun = 9.30 
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10 
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+console.log(euroDun)
+
+let konutKredileri = ["konut kredisi","emlak kredisi","Özel Konut Kredisi","Kamu konut kredisi"]
+console.log(konutKredileri)
+
+console.log("<ul>")
+for(let i =0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("<ul>") 
